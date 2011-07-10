b98289a8fc854a627b83a9fcd9628ba5585f031e
dan.dar33@gmail.com <dan.dar33@gmail.com@736f1d75-7f67-82b1-00a5-a1ce3e7c257f>
2011-07-10 21:14:52 +0000

    OpenSubtitles - moved from dandar3-xbmc-xbox.googlecode.com

---
+++ trunk/plugins/video/OpenSubtitles/default.py
@@ -0,0 +1,45 @@
+##############################################################################
+#
+# OpenSubtitles - XBMC "video" plugin
+# http://www.opensubtitles.org
+#
+# Version 1.42
+# 
+# Coding by Dan Dar3 
+# http://dandar3.blogspot.com
+#
+#
+# Credits:
+#   * OpenSubtitles.org          [ http://www.opensubtitles.org ]
+#   * Team XBMC @ XBMC.org       [ http://xbmc.org ]
+#
+
+# 
+# Constants
+#
+__plugin__  = "OpenSubtitles"
+__author__  = "Dan Dar3"
+__url__     = "http://dandar3.blogspot.com"
+__date__    = "17 October 2010"
+__version__ = "1.42"
+
+#
+# Imports
+#
+import os
+import sys
+
+LIB_DIR = xbmc.translatePath( os.path.join( os.getcwd(), 'resources', 'lib' ) )
+sys.path.append (LIB_DIR)
+
+#
+# Main block
+#
+if ( "action=search" in sys.argv[ 2 ] ):
+    import opensubtitles_search   as plugin
+elif ( "action=download" in sys.argv[ 2 ] ):
+    import opensubtitles_download as plugin
+else:
+    import opensubtitles_main     as plugin
+
+plugin.Main()
---
+++ trunk/plugins/video/OpenSubtitles/readme.txt
@@ -0,0 +1,24 @@
+OpenSubtitles
+~~~~~~~~~~~~~
+
+v1.42 (17 October 2010):
+ - episode subtitle named incorrectly (thanks to nomorgan);
+ - allow browsing for .m2ts files (thanks to matej.jurkic);
+
+v1.4 (1 March 2010):
+ - new server for XML-RPC calls (api.subtitles.org);
+ - set subtitle for playing video;
+
+v1.3 (18 September 2009):
+ - [xbox] if subtitle filename including language is too long, try again without language;
+
+v1.2 (5 July 2009):
+  - add "Search for playing movie";
+  - add language to the subtitle file name;
+
+v1.1 (31 May 2009):
+ - allow to choose preferred subtitle language(s);
+ - allow to choose subtitles destination (movie directory or XBMC custom directory);
+ - rename subtitle(s) to match video file(s);
+ - enhance results list view (number of downloads);
+ - better resolution flags;
